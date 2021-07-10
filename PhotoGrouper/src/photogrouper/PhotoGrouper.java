2032510fee3344268a0591b2cb9c74ec3f8496a7
David <drs@a.recife.ifpe.edu.br>
2021-07-10 00:22:47 -0300

    Adiciona iteração sobre arquivos

--- PhotoGrouper/src/photogrouper/PhotoGrouper.java
+++ PhotoGrouper/src/photogrouper/PhotoGrouper.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package photogrouper;
 
 /**
@@ -11,11 +6,9 @@ package photogrouper;
  */
 public class PhotoGrouper {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+     
+        
+        PhotoController photoController = new PhotoController("C:\\Users\\David\\Desktop\\TestFolder");
+    }   
 }
