462c010a4db6b5394201f314655300eb7a3050fc
Oséias <oseas-ramos123@hotmail.com>
2021-07-10 00:54:06 -0300

    adiciona mover arquivo

--- PhotoGrouper/src/photogrouper/PhotoGrouper.java
+++ PhotoGrouper/src/photogrouper/PhotoGrouper.java
@@ -1,5 +1,7 @@
 package photogrouper;
 
+import java.io.File;
+
 /**
  *
  * @author David
@@ -9,8 +11,13 @@ public class PhotoGrouper {
     public static void main(String[] args) {
        
         FolderManager s = new FolderManager();
+        String destino = "C:\\Users\\user\\Desktop";
+        String nome = "2020";
+        s.criar(destino, nome);
+        s.criar("C:\\Users\\user\\Desktop"+"\\"+2020, "julho");
+        File f = new File("C:\\Users\\user\\Documents\\imagem.jpg");
         
-        s.criar("C:\\Users\\user\\Desktop","teste");
+        s.moverarquivo(destino+"\\"+nome+"\\"+"julho",f);
         
     }
     
