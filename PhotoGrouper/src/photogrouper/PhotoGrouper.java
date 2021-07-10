4c7dfac42364931a1a5396160d5c9e1d5ef3329b
David <drs@a.recife.ifpe.edu.br>
2021-07-10 02:32:58 -0300

    Remove deadcode

--- PhotoGrouper/src/photogrouper/PhotoGrouper.java
+++ PhotoGrouper/src/photogrouper/PhotoGrouper.java
@@ -9,25 +9,9 @@ import java.io.File;
 public class PhotoGrouper {
 
     public static void main(String[] args) {
-
-       
         
-//        File f = new File("C:\\Users\\user\\Desktop\\2020\\julho\\imagem.jpg");;
-//        
-//        FileWrapper w = new FileWrapper(f);
-
-   
-        
-        PhotoController photoController = new PhotoController("C:\\Users\\David\\Desktop\\TestFolder");
-        
+        PhotoController photoController = new PhotoController("C:\\Users\\David\\Desktop\\TestFolder");       
         photoController.groupFiles();
         
-        
-        //FolderManager s = new FolderManager();
-        
-        //s.criar("C:\\Users\\user\\Desktop","teste");
-        
     }
-    
-
 }
